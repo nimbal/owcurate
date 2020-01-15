0e7d1bff78fc00a821fd631bb32f2f4f1c51a5b2
Adam Vert <47859551+VertAdam@users.noreply.github.com>
2020-01-15 14:00:17 -0500

    Delete Shell.py

--- Files/Shell.py
+++
@@ -1,7 +0,0 @@
-from Files.GENEActivFile import GENEActivFile
-from Files.ga_to_edf import ga_to_edf
-
-
-#geneactivfile = GENEActivFile(r"C:\Users\ahvert\PycharmProjects\owcurate\DataFiles_DoNotPush\OND05_SWP_1001_01_GA_LWrist.bin")
-#geneactivfile.read(geneactivfile)
-ga_to_edf("..\\DataFiles_DoNotPush\\KW4_LAnkle.bin","..\\DataFiles_DoNotPush\\Accelerometer","..\\DataFiles_DoNotPush\\Temperature","..\\DataFiles_DoNotPush\\Light","..\\DataFiles_DoNotPush\\Button")
