c125d660be6e05168105c5e8a9f26222fd418b24
Matt Wong <matt@Godfreds-MacBook-Air.local>
2020-06-11 11:21:48 -0400

    Add functionality to update tabular data as well in update to OND06

--- Files/update_to_OND06.py
+++ Files/update_to_OND06.py
@@ -7,18 +7,40 @@ from tqdm import tqdm
 def update_GA_to_OND06(data_pkg_dir, subplatform_code='GNAC'):
     location_mapping = { 'LA': 'LAnkle', 'RA': 'RAnkle', 'LW': 'LWrist', 'RW': 'RWrist', 'AA': 'Aid' }
     edf_files = list(Path(data_pkg_dir).rglob("*.[eE][dD][fF]"))
+    csv_files = list(Path(data_pkg_dir).rglob("*.[cC][sS][vV]"))
+    def update_filename(filename):
+        filename, ext = filename.split('.')
+        s_filename = filename.split('_')
+        subject = s_filename[:3]
+        device = s_filename[-2].upper()
+        location = location_mapping[s_filename[-1]] if s_filename[-1] in location_mapping else s_filename[-1] + '.%s' % ext
+        new_filename = '_'.join(subject + [subplatform_code, device, location]) + '.%s' % ext
+        return new_filename
+
     for f in tqdm(edf_files,desc='Updating Files:', total=len(edf_files)):
         file_dir ,filename = os.path.split(f)
-        filename, ext = filename.split('.')
-
-        s_filename = filename.split('_')
-        new_filename = '_'.join(s_filename[:3] + [subplatform_code,
-                                        s_filename[-2].upper(),
-                                        location_mapping[s_filename[-1]] if s_filename[-1] in location_mapping else s_filename[-1]]) + '.%s' % ext
+        new_filename = update_filename(filename)
         new_f = os.path.join(file_dir, new_filename)
         os.rename(f, new_f)
         update_GA_edf_header(new_f)
-        
+
+    for f in csv_files:
+        df = pd.read_csv(f)
+        if 'VISIT' in df.columns:
+            df['VISIT'] = '01'
+        if 'DEVICE_LOCATION' in df.columns:
+            df['DEVICE_LOCATION'] = df['DEVICE_LOCATION'].apply(lambda x: location_mapping[x] if x in location_mapping else x)
+        if 'FILENAME' in df.columns:
+            df['FILENAME'] = df['FILENAME'].apply(update_filename)
+
+        f = f.replace('Accelerometer', 'ACCELEROMETER'
+            ).replace('Button', 'BUTTON'
+            ).replace('Light', 'LIGHT'
+            ).replace('Temperature','TEMPERATURE'
+            ).replace('GENEActiv', 'GNAC')
+
+        df.to_csv(f, mode="w", index=False)
+        df.close()
 
 
 def update_GA_edf_header(edf_file, update_location=None):
